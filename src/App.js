39b3f767814b69cf377e99d4530265b105c6fb3f
wulan marsuri <marsuriwulan@gmail.com>
2022-05-20 09:45:54 +0700

    initial file hari sabtu

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import {useState} from 'react';
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+      <main>
+      
+      </main>
+    </div>
+ 
+  );
+}
+
+export default App;
